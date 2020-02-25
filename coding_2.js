8e741ffcd3630930315acadccbc650d554d70a4b
Mamata Nayak <59820803+Mamata24@users.noreply.github.com>
2020-02-25 16:25:19 +0530

    W5 D2 E1 C2

---
+++ coding_2.js
@@ -0,0 +1,250 @@
+var stock = ['333.50', '247.60', '1024.50', '595.60', '626.30', '345', '208', '430', '298.60', '729', '339.50', '300', '1027.50', '532.60', '276.30', '343', '300', '450', '243.60', '721', '314.50', '232.60', '924.50', '592.60', '622.30', '324', '226', '455', '294.60', '829'];
+function Day1() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[0];
+        n.appendChild(stocks)
+    }
+}
+function Day2() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[1];
+        n.appendChild(stocks)
+    }
+}
+function Day3() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[2];
+        n.appendChild(stocks)
+    }
+}
+function Day4() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[3];
+        n.appendChild(stocks)
+    }
+}
+function Day5() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[4];
+        n.appendChild(stocks)
+    }
+}
+function Day6() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[5];
+        n.appendChild(stocks)
+    }
+}
+function Day7() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[6];
+        n.appendChild(stocks)
+    }
+}
+function Day8() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[7];
+        n.appendChild(stocks)
+    }
+}
+function Day9() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[8];
+        n.appendChild(stocks)
+    }
+}
+function Day10() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[9];
+        n.appendChild(stocks)
+    }
+}
+function Day11() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[10];
+        n.appendChild(stocks)
+    }
+}
+function Day12() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[11];
+        n.appendChild(stocks)
+    }
+}
+function Day13() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[12];
+        n.appendChild(stocks)
+    }
+}
+function Day14() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[13];
+        n.appendChild(stocks)
+    }
+}
+function Day15() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[14];
+        n.appendChild(stocks)
+    }
+}
+function Day16() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[15];
+        n.appendChild(stocks)
+    }
+}
+function Day17() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[16];
+        n.appendChild(stocks)
+    }
+}
+function Day18() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[17];
+        n.appendChild(stocks)
+    }
+}
+function Day19() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[18];
+        n.appendChild(stocks)
+    }
+}
+function Day20() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[19];
+        n.appendChild(stocks)
+    }
+}
+function Day21() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[20];
+        n.appendChild(stocks)
+    }
+}
+function Day22() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[21];
+        n.appendChild(stocks)
+    }
+}
+function Day23() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[22];
+        n.appendChild(stocks)
+    }
+}
+function Day24() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[23];
+        n.appendChild(stocks)
+    }
+}
+function Day25() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[24];
+        n.appendChild(stocks)
+    }
+}
+function Day26() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[25];
+        n.appendChild(stocks)
+    }
+}
+function Day27() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[26];
+        n.appendChild(stocks)
+    }
+}
+function Day28() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[27];
+        n.appendChild(stocks)
+    }
+}
+function Day29() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[28];
+        n.appendChild(stocks)
+    }
+}
+function Day30() {
+    var n = document.getElementById("ninja");
+    for (i = 0; i < 1; i++) {
+        var stocks = document.createElement('div');
+        stocks.textContent = stock[29];
+        n.appendChild(stocks)
+    }
+}
+function findbuttons() {
+    var buttons = document.getElementById("summon").value
+    switch (buttons) {
+        case 'Greater than':
+            if (stock[i] > 300)
+                alert("Day 1", "Day3", "Day 4", "Day 5", "Day 6");
+            break;
+    }
+}
